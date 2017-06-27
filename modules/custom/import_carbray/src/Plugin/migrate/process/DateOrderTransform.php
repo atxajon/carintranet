6c830a0f04cc52ab78f461301be3445f4a11c309
Jon Atxa <atxajon@gmail.com>
2017-06-27 22:41:36 +0200

    added custom import_carbray module; changed expedientes imported format date definition to transform from d/m/Y to Y-m-d

---
+++ modules/custom/import_carbray/src/Plugin/migrate/process/DateOrderTransform.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\import_cabray\Plugin\migrate\process;
+
+use Drupal\migrate\Row;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+
+/**
+  * NOTE: this plugin is not found and errors if used,
+  * it is not needed as I found format_date plugin,
+  * but I am leaving it here to showcase directions for
+  * next needed custom Plugin.
+  *
+  * @MigrateProcessPlugin(
+  *   id = "transform_date_order"
+  * )
+  */
+class DateOrderTransform extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (is_null($value)) {
+      return $value;
+    }
+
+    $date_bits = explode("/", $value);
+    $new_value = $date_bits[2] . '-' . $date_bits[1] . '-' . $date_bits[0];
+
+    return $new_value;
+  }
+}
