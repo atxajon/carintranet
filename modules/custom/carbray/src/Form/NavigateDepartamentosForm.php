dd11a636be88b02fc01ba9d1f97e1b18bf5f248d
Jon Atxa <atxajon@gmail.com>
2017-07-27 18:02:05 +0200

    departamento page has now a select list with navigation to other departamentos

---
+++ modules/custom/carbray/src/Form/NavigateDepartamentosForm.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\carbray\Form\NewClientForm.
+ */
+namespace Drupal\carbray\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * NavigateDepartamentosForm form.
+ */
+class NavigateDepartamentosForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'carbray_departamentos_dropdown';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+
+    $db = \Drupal::database();
+    // @todo: remove hardcoded and obtain it as parameter.
+    $tid = 38;
+
+    // Display drodpown to navigate to other Departamentos.
+    $sql = "SELECT tid FROM taxonomy_term_field_data WHERE vid= 'departamento' AND tid != :current_tid";
+    $departamentos_tids = $db->query($sql, array(':current_tid' => $tid))->fetchCol();
+
+    $departamento_terms = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadMultiple($departamentos_tids);
+
+    foreach ($departamento_terms as $departamento_term) {
+      $options[$departamento_term->id()] = $departamento_term->name->value;
+    }
+
+    $form['departamentos'] = array(
+      '#type' => 'select',
+      '#options' => $options,
+      '#empty_option' => ' - Selecciona departamento - ',
+      '#attributes' => array('onChange' => 'document.getElementById("carbray-departamentos-dropdown").submit();')
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Go'),
+      '#attributes' => array('class' => array('hide')),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $departamento_tid = $form_state->getValue('departamentos');
+    $url = Url::fromRoute('entity.taxonomy_term.canonical', array('taxonomy_term' => $departamento_tid));
+    $form_state->setRedirectUrl($url);
+  }
+}
