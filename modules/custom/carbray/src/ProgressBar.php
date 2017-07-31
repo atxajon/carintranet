d33bbb2ab36022b83cfbc8e1d053ab61ef04f2f2
Jon Atxa <atxajon@gmail.com>
2017-07-31 15:32:21 +0200

    added more methods to ProgressBar class and unified its usage

--- modules/custom/carbray/src/ProgressBar.php
+++ modules/custom/carbray/src/ProgressBar.php
@@ -5,11 +5,7 @@ namespace Drupal\carbray;
 class ProgressBar {
 
   private $progress_entity;
-  private $now;
-  public $db;
   public $cifra;
-  public $fecha_inicio;
-  public $fecha_final;
 
   public function __construct($type_of_entity) {
     $this->progress_entity = $type_of_entity;
@@ -52,8 +48,40 @@ ff.field_objetivo_fecha_final_value as fecha_final
     $objetivo = $this->db->query($sql, array(':uid' => $uid, ':now' => $this->now))->fetchAll();
 
     $this->cifra = $objetivo[0]->cifra;
-    $this->fecha_inicio = $objetivo[0]->fecha_inicio;
-    $this->fecha_final = $objetivo[0]->fecha_final;
+    $this->fecha_inicio = $this->formatDate($objetivo[0]->fecha_inicio);
+    $this->fecha_final = $this->formatDate($objetivo[0]->fecha_final);
+  }
+
+  /**
+   * Transforms 2017-12-31T22:59:59 into 31-12-2017.
+   *
+   * @param $date
+   *   datetime value.
+   * @param string $format
+   * @return bool|string
+   */
+  public function formatDate($date, $format = "d-M-Y") {
+    $timestamp = strtotime($date);
+    return date($format, $timestamp);
+  }
+
+  public function buildArray() {
+    // @todo: need method to work out total facturado.
+    $total_facturas = 3000.12;
+    $total_facturas = (float)$total_facturas;
+    $percent = $total_facturas / $this->cifra * 100;
+    // Don't let percent exceed 100% when objetivo is achieved.
+    $percent = ($percent > 100) ? 100 : $percent;
+
+    $build = array(
+      '#theme' => 'carbray_progress_bar',
+      '#animate' => FALSE,
+      '#large' => TRUE,
+      '#percent' => $percent,
+      '#objetivo_cifra' => $this->cifra,
+      '#facturado' => $total_facturas,
+    );
+    return $build;
   }
 
 }
