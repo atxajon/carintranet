659884212e98f2b4c35d9e56c55778ad225e6d31
Jon Atxa <atxajon@gmail.com>
2017-08-29 15:46:54 +0200

    created clientes en produccion as a block

---
+++ modules/custom/carbray_cliente/src/Plugin/Block/ClientesProduccion.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Drupal\carbray_cliente\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides a 'ClientesProduccion' block.
+ *
+ * @Block(
+ *  id = "clientes_produccion",
+ *  admin_label = @Translation("Clientes produccion"),
+ * )
+ */
+class ClientesProduccion extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $logged_in_uid = \Drupal::currentUser()->id();
+
+    $sql = "SELECT ufd.uid AS uid, ufd.mail AS mail, n.field_nombre_value AS field_nombre_value, a.field_apellido_value AS field_apellido_value, c.field_captador_target_id AS field_captador_target_id, r.field_responsable_target_id AS field_responsable_target_id, t.field_telefono_value AS field_telefono_value, p.field_pais_value AS field_pais_value, fa.field_fecha_alta_value
+FROM
+users_field_data ufd
+LEFT JOIN user__field_nombre n ON n.entity_id = ufd.uid
+LEFT JOIN user__field_apellido a ON a.entity_id = ufd.uid
+LEFT JOIN user__field_captador c ON c.entity_id = ufd.uid
+LEFT JOIN user__field_responsable r ON r.entity_id = ufd.uid
+LEFT JOIN user__field_fecha_alta fa ON fa.entity_id = ufd.uid
+LEFT JOIN user__field_telefono t ON t.entity_id = ufd.uid
+LEFT JOIN user__field_pais p ON p.entity_id = ufd.uid
+LEFT JOIN user__field_fase f ON f.entity_id = ufd.uid
+WHERE (f.field_fase_value = 'produccion') AND ((c.field_captador_target_id = :logged_in_uid) OR (r.field_responsable_target_id = :logged_in_uid))";
+
+    // GROUP BY uid, mail, field_nombre_value, field_apellido_value, field_captador_target_id,r.field_responsable_target_id, fa.field_fecha_alta_value, field_telefono_value, field_pais_value
+
+    $clientes_produccion = db_query($sql, array(':logged_in_uid' => $logged_in_uid))->fetchAll();
+
+    $already_in_list = array();
+    foreach ($clientes_produccion as $cliente_produccion) {
+      if (!in_array($cliente_produccion->uid, $already_in_list)) {
+        $already_in_list[] = $cliente_produccion->uid;
+        // Build column 'Nombre': link to user page with nombre and apellido as text.
+        $nombre = ($cliente_produccion->field_nombre_value) ? $cliente_produccion->field_nombre_value : ' ';
+        $apellido = ($cliente_produccion->field_apellido_value) ? $cliente_produccion->field_apellido_value : ' ';
+        $nombre_apellido = $nombre . ' ' . $apellido;
+        $url = Url::fromRoute('entity.user.canonical', array('user' => $cliente_produccion->uid));
+        $user_link = Link::fromTextAndUrl($nombre_apellido, $url);
+
+        $contacto = $cliente_produccion->mail . '<br>' . $cliente_produccion->field_telefono_value . '<br>' . $cliente_produccion->field_pais_value;
+
+        /**
+         * A client can have multiple captadores assigned;
+         * Each captador is a link to the Carbray worker.
+         * To print the links in a table $row we need to:
+         *  1- Create the Link obj.
+         *  2- Get its html.
+         *  3- Loop through extra captadores and concatenate their link html in a string.
+         *  4- Create the Markup obj that later will get rendered as HTML by twig.
+         **/
+        if ($cliente_produccion->field_captador_target_id) {
+          $captador = get_cliente_nombre($cliente_produccion->field_captador_target_id);
+          // Step 1:
+          $captador_link = Link::fromTextAndUrl($captador, Url::fromRoute('entity.user.canonical', array('user' => $cliente_produccion->field_captador_target_id), array('attributes' => array('target' => '_blank'))));
+          // Step 2: Convert captador link from Link obj to html.
+          $captador_link_string = $captador_link->toString()->getGeneratedLink();
+          // Does this client have more captadores? add them.
+          $sql = "SELECT field_captador_target_id
+FROM user__field_captador
+WHERE entity_id = :cliente_uid AND field_captador_target_id != :captador";
+          $captadores_extra = db_query($sql, array(':cliente_uid' => $cliente_produccion->uid, ':captador' => $cliente_produccion->field_captador_target_id))->fetchAll();
+          if ($captadores_extra) {
+            foreach ($captadores_extra as $captador_extra) {
+              $captador_extra_name = get_cliente_nombre($captador_extra->field_captador_target_id);
+              $captador_extra_link = Link::fromTextAndUrl($captador_extra_name, Url::fromRoute('entity.user.canonical', array('user' => $captador_extra->field_captador_target_id), array('attributes' => array('target' => '_blank'))));
+              // Step 3:
+              $captador_link_string .= '<br>' . $captador_extra_link->toString()->getGeneratedLink();
+            }
+          }
+        }
+        // Step 4:
+        $captador_markup = Markup::create($captador_link_string);
+        
+        // Print responsable name with link with target _blank.
+        $responsable_link = '';
+        if ($cliente_produccion->field_responsable_target_id) {
+          $responsable = get_cliente_nombre($cliente_produccion->field_responsable_target_id);
+          $responsable_link = Link::fromTextAndUrl($responsable, Url::fromRoute('entity.user.canonical', array('user' => $cliente_produccion->field_captador_target_id), array('attributes' => array('target' => '_blank'))));
+        }
+
+        $rows[] = array(
+          $user_link,
+          $captador_markup,
+          $responsable_link,
+          $cliente_produccion->field_fecha_alta_value,
+          $contacto,
+        );
+      }
+    }
+
+    $header = array(
+      'Nombre',
+      'Captador',
+      'Responsable',
+      'Fecha alta',
+      'Contacto',
+    );
+    $build = array(
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    );
+    return $build;
+  }
+}
