389879563016ed66a6ff81b19d3a43251737f86c
Jon Atxa <atxajon@gmail.com>
2017-10-07 23:29:52 +0200

    modified propuestas so that they download a plantilla node as doc, not create a new propuesta node through a dedicated form

--- modules/custom/carbray_cliente/src/Form/PropuestaPlantillaRef.php
+++ modules/custom/carbray_cliente/src/Form/PropuestaPlantillaRef.php
@@ -22,6 +22,7 @@ class PropuestaPlantillaRef extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['#attributes']['class'][] = 'margin-top';
     $db = \Drupal::database();
     $sql = "SELECT nid, title FROM node_field_data nfd WHERE type = 'plantilla_propuesta'";
     $propuesta_plantillas = $db->query($sql)->fetchAllKeyed();
@@ -32,24 +33,10 @@ class PropuestaPlantillaRef extends FormBase {
       '#options' => $propuesta_plantillas,
     ];
 
-    // Capture current captacion this propuesta is going to refer to on a hidden field.
-    $current_path = \Drupal::service('path.current')->getPath();
-    $path_args = explode('/', $current_path);
-    $captacion_nid = 0;
-    foreach ($path_args as $path_arg) {
-      if (is_numeric($path_arg)) {
-        $captacion_nid = $path_arg;
-      }
-    }
-    $form['captacion_nid'] = [
-      '#type' => 'hidden',
-      '#value' => $captacion_nid,
-    ];
-
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Crear propuesta'),
-      '#attributes' => array('class' => array('btn-sm')),
+      '#value' => $this->t('Descargar propuesta'),
+      '#attributes' => array('class' => array('btn-sm', 'btn-primary')),
     ];
 
     return $form;
@@ -67,14 +54,8 @@ class PropuestaPlantillaRef extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $propuesta_plantilla_nid = $form_state->getValue('propuesta_ref');
-    $captacion_nid = $form_state->getValue('captacion_nid');
-    $route = 'carbray_cliente.new_propuesta';
-    $options = array(
-      'prop_plantilla' => $propuesta_plantilla_nid,
-      'nid' => $captacion_nid,
-    );
-
-    $form_state->setRedirect($route, $options);
-
+    $route = 'carbray_cliente.propuesta_to_doc';
+    $route_parameters = array('nid' => $propuesta_plantilla_nid);
+    $form_state->setRedirect($route, $route_parameters);
   }
 }
