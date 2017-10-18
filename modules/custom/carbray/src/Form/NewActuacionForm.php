2626d4b08ebe47e9bd7cccba0820e6e6ee08cd7b
Jon Atxa <atxajon@gmail.com>
2017-10-18 23:10:03 +0200

    added new actuacion form to expediente node

---
+++ modules/custom/carbray/src/Form/NewActuacionForm.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\carbray\Form\NewExpedienteForm.
+ */
+namespace Drupal\carbray\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\user\Entity\User;
+
+
+/**
+ * NewActuacionForm form.
+ */
+class NewActuacionForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'carbray_new_actuacion';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $expediente_nid = 0) {
+    // Disable caching on this form.
+    $form_state->setCached(FALSE);
+
+    $form['expediente_nid'] = array(
+      '#type' => 'hidden',
+      '#default_value' => $expediente_nid,
+    );
+
+    $form['title'] = array(
+      '#title' => 'Actuacion',
+      '#type' => 'textfield',
+      '#required' => TRUE,
+    );
+
+    $form['timer'] = array(
+      '#title' => 'Clock',
+      '#type' => 'textfield',
+      '#required' => TRUE,
+    );
+
+    $form['nota_container'] = array(
+      '#type' => 'container',
+      '#attributes' => array('class' => array('margin-bottom-20')),
+    );
+    $form['nota_container']['nota'] = array(
+      '#type' => 'text_format',
+      '#title' => 'Notas',
+      '#format' => 'basic_html',
+      '#rows' => 5,
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => 'Añadir actuacion',
+      '#attributes' => array('class' => array('btn-primary', 'btn-sm')),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $expediente_nid = $form_state->getValue('expediente_nid');
+    $title = $form_state->getValue('title');
+    $timer = $form_state->getValue('timer');
+    $nota = $form_state->getValue('nota');
+
+    $actuacion = Node::create(['type' => 'actuacion']);
+    $actuacion->set('title', $title);
+    $actuacion->set('field_actuacion_expediente', $expediente_nid);
+    $actuacion->set('field_actuacion_tiempo_en_seg', $timer);
+    $actuacion->enforceIsNew();
+    $actuacion->save();
+
+    $nota_node = Node::create(['type' => 'nota']);
+    $nota_node->set('title', 'Nota para id ' . $expediente_nid . ' creada el ' . date('d-M-Y H:m:s', time()));
+    $nota_node->set('field_nota_nota', $nota);
+    $nota_node->enforceIsNew();
+    $nota_node->save();
+
+
+    drupal_set_message('Actuacion ' . $title . ' ha sido creada');
+  }
+}
