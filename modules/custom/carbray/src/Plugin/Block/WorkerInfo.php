8058e2fb6f700f9b2e1b59e886cc5bbec0838a71
Jon Atxa <atxajon@gmail.com>
2017-11-08 23:23:13 +0100

    added left sidebar worker info block

---
+++ modules/custom/carbray/src/Plugin/Block/WorkerInfo.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\carbray\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\user\Entity\User;
+use Drupal\taxonomy\Entity\Term;
+
+
+
+/**
+ * Provides an WorkerInfo Block.
+ *
+ * @Block(
+ *   id = "worker_info",
+ *   admin_label = @Translation("Worker Info"),
+ *   category = @Translation("Worker Info"),
+ * )
+ */
+class WorkerInfo extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $user = User::load(\Drupal::currentUser()->id());
+    $name = $user->get('field_nombre')->value .  '<br> ' . $user->get('field_apellido')->value;
+    $department_tids = $user->get('field_departamento')->getValue();
+    $department = '';
+    foreach ($department_tids as $department_tid) {
+      $department_term = Term::load($department_tid['target_id']);
+      $department .= $department_term->name->value . ' Dept. <br>';
+    }
+    $build['worker_info'] = [
+      '#markup' => '<div class="worker-info-container"><h1 class="text-center">' . $name . '</h1>' . '<h4 class="text-center">' . $department . '</h4></div>',
+    ];
+    return $build;
+  }
+}
