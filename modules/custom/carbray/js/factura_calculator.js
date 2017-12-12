1f15f41d42098113434be4ad1c7a4eceb1ef5199
Jon Atxa <atxajon@gmail.com>
2017-12-12 23:12:18 +0100

    changed new factura form approach from custom form to default node/add/factura form redirect and paragraphs contrib module

--- modules/custom/carbray/js/factura_calculator.js
+++ modules/custom/carbray/js/factura_calculator.js
@@ -2,32 +2,101 @@
   Drupal.behaviors.factura_calculator = {
     attach: function (context, settings) {
       // Fire calculation on precio input blur event.
-      $('#edit-coste-fieldset-precio').on('blur', function () {
+      $('.field--name-field-factura-servicios .form-number', context).once('factura_calculator').on('blur', function () {
         var precio = parseFloat($(this).val());
-        var iva = $('#new-factura input[type=radio]:checked').val();
+        var acc_total = 0;
         var total = 0;
-        if (iva == 0) {
-          total = precio;
+
+        $('.field--name-field-factura-servicios .form-number').each(function( index ) {
+          acc_total += parseFloat($(this).val());
+        });
+
+        if ($('#edit-field-factura-iva-value').is(':checked')) {
+          total = acc_total + acc_total * 0.21;
         }
         else {
-          total = precio + precio * 0.21;
+          total = acc_total;
         }
-        $('#edit-coste-fieldset-importe-total').val(total.toFixed(2));
+
+        $('#edit-field-factura-precio-0-value').val(total.toFixed(2));
       });
 
       // Fire calculation on iva radio button select change.
-      $('#new-factura input[type=radio]').change(function () {
-        var precio = parseFloat($('#edit-coste-fieldset-precio').val());
-        var iva = $('#new-factura input[type=radio]:checked').val();
+      $('#edit-field-factura-iva-value', context).once('factura_calculator').on('change', function () {
+        var servicios_total = 0;
+        var prov_fondos = 0;
+
+          $('.field--name-field-factura-servicios .form-number').each(function( index ) {
+          servicios_total += parseFloat($(this).val());
+        });
         var total = 0;
-        if (iva == 0) {
-          total = precio;
+
+        if ($('#edit-field-factura-iva-value').is(':checked')) {
+          total = servicios_total + servicios_total * 0.21;
+          prov_fondos = parseFloat($('#edit-field-factura-provision-de-fondo-0-value').val());
+          // If var is undefined (NaN).
+          if (prov_fondos !== prov_fondos) {
+            prov_fondos = 0;
+          }
+          total += prov_fondos;
         }
         else {
-          total = precio + precio * 0.21;
+          total = servicios_total;
+          prov_fondos = parseFloat($('#edit-field-factura-provision-de-fondo-0-value').val());
+          // If var is undefined (NaN).
+          if (prov_fondos !== prov_fondos) {
+            prov_fondos = 0;
+          }
+          total += prov_fondos;
         }
-        $('#edit-coste-fieldset-importe-total').val(total.toFixed(2));
+
+        $('#edit-field-factura-precio-0-value').val(total.toFixed(2));
       });
+
+      // Fire calculation on provision de fondos input blur event.
+      $('#edit-field-factura-provision-de-fondo-0-value', context).once('factura_calculator').on('blur', function () {
+        var servicios_total = 0;
+        $('.field--name-field-factura-servicios .form-number').each(function( index ) {
+          servicios_total += parseFloat($(this).val());
+        });
+
+        var prov_fondos = parseFloat($(this).val());
+        var total = servicios_total + prov_fondos;
+
+        $('#edit-field-factura-precio-0-value').val(total.toFixed(2));
+      });
+
+
+      /**
+       * Old code for NewFacturaFOrm.php, currently unused
+       * as we are favoring using node/add/factura default form.
+       */
+      //$('#edit-coste-fieldset-precio').on('blur', function () {
+      //  var precio = parseFloat($(this).val());
+      //  var iva = $('#new-factura input[type=radio]:checked').val();
+      //  var total = 0;
+      //  if (iva == 0) {
+      //    total = precio;
+      //  }
+      //  else {
+      //    total = precio + precio * 0.21;
+      //  }
+      //  $('#edit-coste-fieldset-importe-total').val(total.toFixed(2));
+      //});
+
+      // Fire calculation on iva radio button select change.
+      //$('#new-factura input[type=radio]').change(function () {
+      //  var precio = parseFloat($('#edit-coste-fieldset-precio').val());
+      //  var iva = $('#new-factura input[type=radio]:checked').val();
+      //  var total = 0;
+      //  if (iva == 0) {
+      //    total = precio;
+      //  }
+      //  else {
+      //    total = precio + precio * 0.21;
+      //  }
+      //  $('#edit-coste-fieldset-importe-total').val(total.toFixed(2));
+      //});
     }
   }
 })(jQuery, Drupal);
