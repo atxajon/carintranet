f4444b8c795d32e4fe4aaa738120633693f54d5a
Jon Atxa <atxajon@gmail.com>
2018-02-13 23:13:51 +0100

    added expediente actuaciones to doc download

--- modules/custom/carbray_cliente/src/Controller/PropuestaToDoc.php
+++ modules/custom/carbray_cliente/src/Controller/PropuestaToDoc.php
@@ -3,6 +3,8 @@
 namespace Drupal\carbray_cliente\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+
 
 /**
  * Class PropuestaToDoc.
@@ -15,14 +17,15 @@ class PropuestaToDoc extends ControllerBase {
   public function getPropuestaNode($nid) {
 
     $entity_type = 'node';
-    $view_mode = 'propuesta_doc';
+    $view_mode = 'pdf';
 
     $view_builder = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
     $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
     $node = $storage->load($nid);
     $build = $view_builder->view($node, $view_mode);
     $output = render($build);
-    $filename = 'propuesta-' . $nid . '.doc';
+    $node = Node::load($nid);
+    $filename = 'expediente-' . $node->label() . '.doc';
     $c_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
 //    $c_type = 'application/vnd.msword';
 //    $c_type = 'application/msword';
@@ -65,6 +68,31 @@ class PropuestaToDoc extends ControllerBase {
       )
     ];
 
+    return $render_array;
+  }
+
+  public function getExpedienteNodeToPdf($nid) {
+    $entity_type = 'node';
+    $view_mode = 'pdf';
+
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
+    $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+    $node = $storage->load($nid);
+    $build = $view_builder->view($node, $view_mode);
+    $output = render($build);
+    $filename = 'expediente-' . $nid . '.txt';
+//    $c_type = 'application/pdf';
+//    $c_type = 'application/octet-stream';
+    $c_type = 'text/plain';
+    $render_array = [
+      '#markup' => $output,
+//      '#attached' => array(
+//        'http_header' => array(
+//          array('Content-Type', $c_type),
+//          array('content-disposition',  "attachment;filename=$filename"),
+//        )
+//      )
+    ];
     return $render_array;
   }
 
