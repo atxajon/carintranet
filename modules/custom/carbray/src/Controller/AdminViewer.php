ee7cc30e91166dfb5e24cb6557d1df5b132fceba
Jon Atxa <atxajon@gmail.com>
2018-02-19 21:45:02 +0100

    added clientes produccion expedientes block view as admin viewer

--- modules/custom/carbray/src/Controller/AdminViewer.php
+++ modules/custom/carbray/src/Controller/AdminViewer.php
@@ -11,11 +11,20 @@ class AdminViewer extends ControllerBase {
 
   public function WorkerHome($uid) {
     $clientes = get_my_clients($uid);
-    $build['prefix'] = [
-      '#markup' => '<div class="block"><h2>Contactos en captacion</h2>',
+    $build['captacion_prefix'] = [
+      '#markup' => '<div class="block margin-bottom-20"><h2>Contactos en captacion</h2>',
     ];
     $build['contactos_captacion'] = contactos_captacion_content($clientes);
     $build['suffix'] = [
+      '#markup' => '</div>',
+    ];
+    $build['expedientes_prefix'] = [
+      '#markup' => '<div class="block"><h2>Clientes en produccion</h2>',
+    ];
+
+    $clientes = get_my_clients($uid, 'produccion');
+    $build['contactos_expedientes'] = clientes_expedientes_content($clientes);
+    $build['expedientes_suffix'] = [
       '#markup' => '</div>',
     ];
     return $build;
--- modules/custom/carbray_cliente/src/Plugin/Block/ClientesProduccion.php
+++ modules/custom/carbray_cliente/src/Plugin/Block/ClientesProduccion.php
@@ -20,59 +20,7 @@ class ClientesProduccion extends BlockBase {
   public function build() {
     $logged_in_uid = \Drupal::currentUser()->id();
     $clientes = get_my_clients($logged_in_uid, 'produccion');
-    $rows = [];
-    foreach ($clientes as $cliente) {
-      $cliente_data = \Drupal::entityTypeManager()->getStorage('user')->load($cliente->uid);
-      if (!$cliente_data) {
-        continue;
-      }
-
-      $captacion_data = \Drupal::entityTypeManager()->getStorage('node')->load($cliente->captacion_nid);
-      $estado_captacion = $captacion_data->get('field_captacion_estado_captacion')->entity;
-      if ($estado_captacion->id() == CAPTACION_ARCHIVADA) {
-        continue;
-      }
-
-      $expedientes = get_expedientes_for_captacion($cliente->captacion_nid);
-      if (!$expedientes) {
-        continue;
-      }
-      $expedientes_nids = array_values($expedientes);
-
-      // @todo: add logic to allow for multiple captaciones for this cliente...
-      $expediente_data = \Drupal::entityTypeManager()->getStorage('node')->load($expedientes_nids[0]);
-
-      $tematicas = $expediente_data->get('field_expediente_tematica')->getValue();
-      $tematica = reset($tematicas);
-
-      $rows[] = array(
-        print_cliente_link($cliente_data),
-        print_cliente_captadores_responsables($captacion_data->get('field_captacion_captador')->getValue()),
-        print_cliente_captadores_responsables($expediente_data->get('field_expediente_responsable')->getValue()),
-        print_cliente_tematica($tematica),
-        ($cliente_data->getEmail()) ? $cliente_data->getEmail() : '',
-        ($cliente_data->get('field_telefono')->value) ? $cliente_data->get('field_telefono')->value : '',
-        print_cliente_expedientes($expedientes_nids),
-      );
-    }
-
-    $header = array(
-      'Nombre',
-      'Captador',
-      'Responsable',
-      'Tematica',
-      'Email',
-      'Telefono',
-      'Expedientes',
-    );
-    $build = array(
-      '#type' => 'table',
-      '#header' => $header,
-      '#rows' => $rows,
-      '#empty' => t('Ningun cliente en produccion.'),
-    );
-    // Disable caching...
-    $build['#cache']['max-age'] = 0;
+    $build = clientes_expedientes_content($clientes);
     return $build;
   }
 }
