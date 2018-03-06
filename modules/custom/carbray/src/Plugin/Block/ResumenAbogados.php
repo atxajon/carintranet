0716dfec047340fa6856dd999ca0b5c8a9351f14
Jon Atxa <atxajon@gmail.com>
2018-03-06 16:59:48 +0100

    made count of expedientes archived dynamic on resumen abogados

--- modules/custom/carbray/src/Plugin/Block/ResumenAbogados.php
+++ modules/custom/carbray/src/Plugin/Block/ResumenAbogados.php
@@ -49,7 +49,7 @@ ORDER BY field_apellido_value ASC')->fetchAll();
       $count_captaciones_activas = get_count_captaciones_activas($worker->uid);
       $count_captaciones_archivadas = get_count_captaciones_archivadas($worker->uid, $query_array);
       $count_expedientes_published = get_count_expedientes_published($worker->uid, $query_array);
-      $count_expedientes_archived = get_count_expedientes_archived($worker->uid);
+      $count_expedientes_archived = get_count_expedientes_archived($worker->uid, $query_array);
       $count_facturas_emitidas = get_count_facturas_emitidas($worker->uid);
       $count_facturas_pagadas = get_count_facturas_pagadas($worker->uid);
 
