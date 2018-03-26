ea1806676fa7d572b7ae45c058b94547d2db5f2f
Jon Atxa <atxajon@gmail.com>
2018-03-26 18:23:10 +0200

    added department filtering to calendar actuaciones

--- modules/custom/carbray_calendar/src/Plugin/Block/FullCalendar.php
+++ modules/custom/carbray_calendar/src/Plugin/Block/FullCalendar.php
@@ -28,31 +28,32 @@ class FullCalendar extends BlockBase {
       '#markup' => render($form),
     ];
 
-
-    $nids = \Drupal::entityQuery('node')->condition('type','actuacion')->execute();
-    $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+    $actuaciones = \Drupal::database()->query("SELECT field_actuacion_tiempo_en_seg_value as minutes, nid, field_actuacion_expediente_target_id as expediente_nid, created, title, field_departamento_target_id as departamento_tid
+FROM node_field_data nfd
+INNER JOIN node__field_actuacion_expediente ac on nfd.nid = ac.entity_id
+INNER JOIN node__field_actuacion_tiempo_en_seg t on nfd.nid = t.entity_id
+INNER JOIN user__field_departamento d on nfd.uid = d.entity_id
+WHERE type = 'actuacion'")->fetchAll();
     $data = [];
-    foreach ($nodes as $node) {
-      $expediente_nid = $node->get('field_actuacion_expediente')->getValue();
+    foreach ($actuaciones as $actuacion) {
 
       // Work out actuacion start and end date;
       // We only have actuacion form submission time (node created value in timestamp)
       // and minutes passed declared by the user.
       // Start and end date can be misleading as often times a worker submits 2 actuaciones for say 30 mins one after the other.
       // They plot on the calendar as overlapping time...
-      $actuacion_minutes = $node->get('field_actuacion_tiempo_en_seg')->value;
+      $actuacion_minutes = $actuacion->minutes;
       $actuacion_seconds = $actuacion_minutes * 60;
-      $actuacion_created = $node->created->value;
+      $actuacion_created = $actuacion->created;
       $actuacion_started = $actuacion_created - $actuacion_seconds;
 
-      // @todo: get expediente responsable's department.
       $data[] = [
-        'title' => $node->label(),
+        'title' => $actuacion->title,
         'start' => date("c", $actuacion_started), //converting tiemstamp to ISO 8601 https://stackoverflow.com/questions/5322285/how-do-i-convert-datetime-to-iso-8601-in-php/5322309
         'end' => date("c", $actuacion_created),
-        'url' => Url::fromRoute('entity.node.canonical', ['node' => $expediente_nid[0]['target_id']]
+        'url' => Url::fromRoute('entity.node.canonical', ['node' => $actuacion->expediente_nid]
         )->toString(),
-        'dept' => 186,
+        'dept' => $actuacion->departamento_tid,
 //        'allDay' => false,
       ];
     }
