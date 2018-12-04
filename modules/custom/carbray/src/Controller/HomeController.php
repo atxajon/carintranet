2b519f209fba9bbd48f70eeb3fe190174eb1b980
Jon Atxa <atxajon@gmail.com>
2018-12-04 18:03:45 +0100

    added home calendars

--- modules/custom/carbray/src/Controller/HomeController.php
+++ modules/custom/carbray/src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\carbray\CsvResponse;
 use Drupal\node\Entity\Node;
 use Drupal\Core\Render\Markup;
+use Drupal\Core\Url;
 
 class HomeController extends ControllerBase {
 
@@ -41,6 +42,90 @@ class HomeController extends ControllerBase {
       '#leads_produccion_count' => $leads_produccion,
       '#facturacion_total' => 10002,
     );
+
+    $current_user_roles = \Drupal::currentUser()->getRoles();
+    if (in_array('administrator', $current_user_roles) || in_array('carbray_administrator', $current_user_roles)) {
+      $colours = get_calendar_colours();
+      $colours_legend = get_calendar_colours_legend($colours);
+    }
+
+    $actuaciones = get_calendar_actuaciones($logged_in_uid);
+    $current_iteration_nid = 0;
+    $data = [];
+    foreach ($actuaciones as $actuacion) {
+
+      /**
+       * The actuaciones sql query returns duplicated data, because a user can be in multiple departments;
+       * If they do they return an actuacion row for each department they're in.
+       * This could be fixed before mysql 5.7 with 'group by = nid', but now they're enforcing ONLY_FULL_GROUP_BY
+       * and each column needs to be thrown into group by clause. Couldn't get it to work,
+       * so an (ugly) workaround is to check if the current iteration nid is already part of the result set,
+       * and if it is -> skip to next row iteration.
+       */
+      if ($current_iteration_nid == $actuacion->nid) {
+        continue;
+      }
+
+      // Work out actuacion start and end date;
+      // We only have actuacion form submission time (node created value in timestamp)
+      // and minutes passed declared by the user.
+      // Start and end date can be misleading as often times a worker submits 2 actuaciones for say 30 mins one after the other.
+      // They plot on the calendar as overlapping time...
+      $actuacion_minutes = $actuacion->minutes;
+      $actuacion_seconds = $actuacion_minutes * 60;
+      $actuacion_created = $actuacion->created;
+      $actuacion_started = $actuacion_created - $actuacion_seconds;
+
+      if (isset($colours[$actuacion->author])) {
+        $colour = (substr($colours[$actuacion->author], 0, 1) === '#') ? $colours[$actuacion->author] : '#' . $colours[$actuacion->author];
+      }
+      else {
+        // Default gray colour for content that has an orphaned author (removed in the system) but still needs to be shown.
+        $colour = '#969696';
+      }
+
+      $data[] = [
+        'title' => $actuacion->title,
+        'start' => date("c", $actuacion_started), // converting tiemstamp to ISO 8601 https://stackoverflow.com/questions/5322285/how-do-i-convert-datetime-to-iso-8601-in-php/5322309
+        'end' => date("c", $actuacion_created),
+        'created' => date('d-m-Y H:m:s', $actuacion->created),
+        'url' => Url::fromRoute('entity.node.canonical', ['node' => $actuacion->expediente_nid]
+        )->toString(),
+        'dept_id' => $actuacion->departamento_tid,
+        'dept' => $actuacion->departamento,
+        'color' => $colour,
+        'author' => $actuacion->nombre . ' ' . $actuacion->apellido,
+        'author_uid' => $actuacion->author,
+        'type' => 'Actuación',
+//        'allDay' => false,
+      ];
+      $current_iteration_nid = $actuacion->nid;
+    }
+
+    // @todo: feed the following calendar with data from citas of category ausencias/vacaciones.
+    $build['actuaciones_calendar'] = [
+      '#markup' => '<div class="row"><div class="col-sm-6"><div class="block"><div id="actuaciones_calendar"></div></div></div>',
+      '#attached' => array(
+        'library' => array(
+          'carbray_calendar/fullcalendar',
+        ),
+        'drupalSettings' => array(
+          'data' => $data,
+        ),
+      ),
+    ];
+
+    $build['ausencias_calendar'] = [
+      '#markup' => '<div class="col-sm-6"><div class="block"><div id="ausencias_calendar"></div></div></div></div>',
+      '#attached' => array(
+        'library' => array(
+          'carbray_calendar/fullcalendar',
+        ),
+        'drupalSettings' => array(
+          'data' => $data,
+        ),
+      ),
+    ];
 
 
 
--- modules/custom/carbray_calendar/js/calendar_custom.js
+++ modules/custom/carbray_calendar/js/calendar_custom.js
@@ -41,6 +41,90 @@ var data = drupalSettings.data;
         }
       });
 
+
+      $('#actuaciones_calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'agendaDay,listDay,agendaWeek,month,listMonth'
+        },
+        defaultView: 'agenda',
+        // defaultDate: '2018-01-20',
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        navLinks: true, // can click day/week names to navigate views
+        events: data,
+        eventRender: function eventRender( event, element, view ) {
+          // Prepare the bootstrap tooltip.
+          $(element).attr('data-html', true); // Allows HTML on tooltip.
+          $(element).tooltip({title: '<h3>' + event.title + '</h3><br />' +
+            '<h4>Creado: ' + event.created + '<br /><br />' + 'Autor: ' + event.author + '<br /><br />' + 'Departamento: ' + event.dept + '<br /><br />' + 'Tipo de evento: ' + event.type + '<br /><br />' +
+            '</h4>', 'placement': 'top'});
+
+          // Filter by department tid.
+          var dept = $('#edit-departamento').val();
+          // If dropdown is set to show all (no filtering) it sends a 0.
+          if (dept > 0) {
+            return ['all', event.dept_id].indexOf($('#edit-departamento').val()) >= 0
+          }
+
+          // Filter by worker uid.
+          var worker_uid = $('#edit-trabajador').val();
+          if (worker_uid > 0) {
+            return ['all', event.author_uid].indexOf($('#edit-trabajador').val()) >= 0
+          }
+        },
+        eventColor: '#378006',
+        // Open up link on a new tab.
+        eventClick: function(event) {
+          window.open(event.url);
+          return false;
+        }
+      });
+
+      $('#ausencias_calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'agendaDay,listDay,agendaWeek,month,listMonth'
+        },
+        defaultView: 'agenda',
+        // defaultDate: '2018-01-20',
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        navLinks: true, // can click day/week names to navigate views
+        events: data,
+        eventRender: function eventRender( event, element, view ) {
+          // Prepare the bootstrap tooltip.
+          $(element).attr('data-html', true); // Allows HTML on tooltip.
+          $(element).tooltip({title: '<h3>' + event.title + '</h3><br />' +
+            '<h4>Creado: ' + event.created + '<br /><br />' + 'Autor: ' + event.author + '<br /><br />' + 'Departamento: ' + event.dept + '<br /><br />' + 'Tipo de evento: ' + event.type + '<br /><br />' +
+            '</h4>', 'placement': 'top'});
+
+          // Filter by department tid.
+          var dept = $('#edit-departamento').val();
+          // If dropdown is set to show all (no filtering) it sends a 0.
+          if (dept > 0) {
+            return ['all', event.dept_id].indexOf($('#edit-departamento').val()) >= 0
+          }
+
+          // Filter by worker uid.
+          var worker_uid = $('#edit-trabajador').val();
+          if (worker_uid > 0) {
+            return ['all', event.author_uid].indexOf($('#edit-trabajador').val()) >= 0
+          }
+        },
+        eventColor: '#378006',
+        // Open up link on a new tab.
+        eventClick: function(event) {
+          window.open(event.url);
+          return false;
+        }
+      });
+
+
+
+
       // On filters change rerender the calendar to take into account passed in filter value.
       $('#edit-departamento').on('change',function(){
         $('#calendar').fullCalendar('rerenderEvents');
@@ -50,5 +134,7 @@ var data = drupalSettings.data;
       });
 
     }
+
+
   }
 })(jQuery, Drupal);
